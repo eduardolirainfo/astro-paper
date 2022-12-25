ad18f4f665ed5f2dd2ed61d1d1e7ff32a400e659
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-25 00:32:57 -0300

    tinacms3

--- .tina/config.ts
+++ .tina/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "content/posts",
+        path: "src/contents",
         fields: [
           {
             type: "string",
