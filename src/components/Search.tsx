9ecec00e41d5a16e2a8bdd09850431356885bb8d
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-31 00:50:53 -0300

    update posts

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -79,7 +79,7 @@ export default function SearchBar({ searchList }: Props) {
           </svg>
         </span>
         <input
-          className="placeholder:italic placeholder:text-opacity-75 py-3 pl-10 pr-3 
+          className="placeholder:italic placeholder:text-opacity-75 py-3 pl-12 pr-3 
         block bg-skin-fill w-full rounded
         border border-skin-fill border-opacity-40 
         focus:outline-none focus:border-skin-accent"
